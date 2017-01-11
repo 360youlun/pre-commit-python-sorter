55d3cf2f63abdbc96c35ff5d6c4d1f0bb30b5960
bin <iamsk.info@gmail.com>
2017-01-11 18:17:26 +0800

    settings_path support

--- pre_commit_hook/sort.py
+++ pre_commit_hook/sort.py
@@ -6,8 +6,8 @@ import os
 from isort import isort
 
 
-def imports_incorrect(filename, show_diff=False):
-    return isort.SortImports(filename, check=True, show_diff=show_diff).incorrectly_sorted
+def imports_incorrect(filename, show_diff=False, settings_path=None):
+    return isort.SortImports(filename, check=True, show_diff=show_diff, settings_path=settings_path).incorrectly_sorted
 
 
 def main(argv=None):
@@ -17,19 +17,20 @@ def main(argv=None):
     parser.add_argument('--silent-overwrite', action='store_true', dest='silent', default=False)
     parser.add_argument('--check-only', action='store_true', dest='check_only', default=False)
     parser.add_argument('--diff', action='store_true', dest='show_diff', default=False)
+    parser.add_argument('--config', dest='settings_path', default=None)
     args = parser.parse_args(argv)
 
     return_value = 0
 
     for filename in args.filenames:
-        if imports_incorrect(filename, show_diff=args.show_diff):
+        if imports_incorrect(filename, show_diff=args.show_diff, settings_path=args.settings_path):
             if args.check_only:
                 return_value = 1
             elif args.silent:
-                isort.SortImports(filename)
+                isort.SortImports(filename, settings_path=args.settings_path)
             else:
                 return_value = 1
-                isort.SortImports(filename)
+                isort.SortImports(filename, settings_path=args.settings_path)
                 print('FIXED: {0}'.format(os.path.abspath(filename)))
     return return_value
 
